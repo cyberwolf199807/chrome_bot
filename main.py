4d02668246064a9906a5709c00c1a5a68808af85
Cyber_wolf <51040820+cyberwolf199807@users.noreply.github.com>
2020-06-15 14:08:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,21 @@ import pyautogui as pag;
 from PIL import Image, ImageGrab
 from numpy import asarray
 import time
-flag=0;
+flag=0
+
 def operation_up():
     pag.keyDown('Up')
-    
 def start():
     time.sleep(5)
 def keep_track(image):
     data1=asarray(image)
     print(Image.fromarray(data1))
     print(data1)
-
 def translate_image(data):
     for i in range(450,564):
         for j in range(225,270):
             data[i,j]=171
     return data
-
 def collide(data):
     for i in range(457,574):
         for j in range(225,270):
@@ -33,8 +31,6 @@ def grayscale_picture():
     data=translate_image(data)
     keep_track(image)
     
-
-
 if __name__=="__main__":
     print("Dino auto Script will start in 5 sec")
     start()
